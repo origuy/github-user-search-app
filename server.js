5554f99f303cc25efce6ab100504e83064775fb3
Ori Guy <origuy2021@gmail.com>
2022-02-14 15:54:12 +0200

    update files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+app.use(express.static(__dirname + '/public'))
 app.get('/',(request, respond) => {
     respond.sendFile(__dirname + '/index.html')
 })
